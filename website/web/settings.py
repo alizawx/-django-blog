3b221dee77b7ded1e67646c22d528c9ec4de2857
alizawx <mralirezanzriii@gmail.com>
2026-07-03 21:17:51 +0330

    connect to postgeSQL

--- website/web/settings.py
+++ website/web/settings.py
@@ -73,22 +73,23 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blog',
-#         'USER':'blog_admin',
-#         'PASSWORD':'123456',
-#         'PORT':'5432',
-#
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER':'blog_admin',
+        'PASSWORD':'123456',
+        'PORT':'5432',
+    }
+}
+
 
 
 
