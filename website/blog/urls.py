e681aedb90704e2bbce5fb1838614d45585d60b7
alizawx <mralirezanzriii@gmail.com>
2026-08-01 19:19:27 +0330

    fixed bug

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path('logout/',views.log_out, name='log_out'),
     path('password-change/', auth_views.PasswordChangeView.as_view(success_url='done'), name='password-change'),
-    path('password-change/down/', auth_views.PasswordChangeDoneView.as_view(), name='password-change-done'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password-change-done'),
 
 
 ]
